8139816977276d81e5aa587ec794ba864295ec5a
Carlton Dickson <carltondickson@Carltons-Mini.lan>
2024-10-10 15:15:42 +0100

    Few updates

--- main.go
+++ main.go
@@ -2,21 +2,44 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
 
 func main() {
+	var err error
+	var files *template.Template
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	files, err = template.ParseFiles("./templates/index.html")
+	if err != nil {
+		return
+	}
+
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "Home page of the test application")
+		data := map[string]string{
+			"Region": os.Getenv("FLY_REGION"),
+		}
+		err = files.Execute(w, data)
+		if err != nil {
+			_, _ = fmt.Fprint(w, err)
+			return
+		}
 	})
+
 	mux.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "The about page of the test application")
 	})
-	err := http.ListenAndServe(":8080", mux)
+
+	err = http.ListenAndServe(":"+port, mux)
 	if err != nil {
-		fmt.Fprint(os.Stderr, err)
+		_, _ = fmt.Fprint(os.Stderr, err)
 		return
 	}
 }
